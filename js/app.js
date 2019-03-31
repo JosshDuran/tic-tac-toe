8c148328edc6cfade3268f3becda7b493f49984f
Jossh Duran <josshduran@Jennifers-MacBook-Pro.local>
2019-03-31 00:56:07 -0700

    final commit

--- js/app.js
+++ js/app.js
@@ -9,20 +9,17 @@ var WIN_CONDITIONS = [
   [2, 4, 6]
 ]
 var turn = 0;
-var counter = 1; // turn number
+var counter = 1;
 var gameBoard = document.getElementsByClassName("grid");
 var startButton = document.getElementById("reset");
 var messages = document.getElementById("msg");
-var player
-
+var player;
 // Add click listener to start button
 startButton.addEventListener('click', reset);
-
-
+// Add start listeners function
 function startListeners() {
   for(var i=0; i< gameBoard.length; i++){
     gameBoard[i].addEventListener('click', showImage)
-    console.log(i)
   }
 }
 
@@ -30,78 +27,69 @@ function showImage(e) {
   // Stop listening to clicks
   this.removeEventListener('click', showImage);
 
-
-
-
   var imagePlayer = document.createElement("img");
 
   if(turn%2){
     imagePlayer.setAttribute("src","img/dog.jpeg");
-    var classElm = "dog"
+    var classElm = "dog";
   }
   else {
     imagePlayer.setAttribute("src","img/cat.jpeg");
-    var classElm = "cat"
+    var classElm = "cat";
   }
 
   event.srcElement.appendChild(imagePlayer);
-  event.srcElement.classList.add(classElm)
+  event.srcElement.classList.add(classElm);
   event.srcElement.setAttribute("player", classElm);
 
-  player = turn % 2 ? 'dog' : 'cat'
+  player = turn % 2 ? 'dog' : 'cat';
   turn++;
   counter++; 
   if(checkWinner()){
-    //The player in your click function will be the winner if checkWinner() returns true
-    console.log("WINNER")
+    console.log("WINNER");
     removeListeners();
-    // endGame function?
-    // Whatever win condition goes here
   }
-  else if(counter >= 9){
-    messages.textContent = "It's a tie!";
+  else if(counter >= 10){
+    messages.textContent = "It's a draw!";
     removeListeners();
   }
 }
 
 function checkWinner() {
-  // Loop through WIN_CONDITIONS
-  var win
+  // Loop through win conditions
+  var win;
   
   for (var i = 0; i < WIN_CONDITIONS.length; i++) {
     if (!gameBoard[WIN_CONDITIONS[i][0]].getAttribute("player") || !gameBoard[WIN_CONDITIONS[i][1]].getAttribute("player") || !gameBoard[WIN_CONDITIONS[i][2]].getAttribute("player")) {
-      win = false
+      win = false;
     } else if (gameBoard[WIN_CONDITIONS[i][0]].getAttribute("player") === gameBoard[WIN_CONDITIONS[i][1]].getAttribute("player") &&
-        gameBoard[WIN_CONDITIONS[i][1]].getAttribute("player") === gameBoard[WIN_CONDITIONS[i][2]].getAttribute("player")) {
-        messages.textContent = "The winner is " + player;
-      win = true
-    } else {
-      win = false
-    }
+               gameBoard[WIN_CONDITIONS[i][1]].getAttribute("player") === gameBoard[WIN_CONDITIONS[i][2]].getAttribute("player")) {
+                  messages.textContent = "The winner is " + player;
+                  win = true;
+      } else {
+        win = false;
+        }
     
     if (win === true) {
-    return win
+      return win;
     }
   }
-
-  return win
+  return win;
 }
-
+// Add remove listeners
 function removeListeners() {
   for(var i=0; i<gameBoard.length; i++){
-    gameBoard[i].removeEventListener('click', showImage)
-    // element.removeAttribute(attributename)
-    gameBoard[i].removeAttribute("player")
-    console.log(i)
+    gameBoard[i].removeEventListener('click', showImage);
+    gameBoard[i].removeAttribute("player");
   }
 }
-
+// Add a reset function
 function reset() {
   for(var i=0; i<gameBoard.length;i++){
     if(!!gameBoard[i].children.length){
       gameBoard[i].removeChild(gameBoard[i].children[0]);
       turn = 0;
-      counter = 1; // turn number
+      counter = 1;
       messages.textContent = "";
     }
   }
